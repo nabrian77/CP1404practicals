b9f16af6d9440a964a2ee40481f6f48b0d41b8ca
Dohoon Kim <nabrian77@gmail.com>
2023-03-25 17:00:57 +0800

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,7 +1,19 @@
-user_password = input("Enter your password: ")
-while user_password == "":
-    print("Please enter your password")
+def main():
+    user_password = get_password()
+    while user_password == "":
+        print("Please enter your password")
+        user_password = get_password()
+    password_length = len(user_password)
+    print_stars(password_length)
+
+
+def print_stars(password_length):
+    for i in range(password_length):
+        print("*", end='')
+
+
+def get_password():
     user_password = input("Enter your password: ")
-password_length = len(user_password)
-for i in range(password_length):
-    print("*", end= '')
+    return user_password
+
+main()
