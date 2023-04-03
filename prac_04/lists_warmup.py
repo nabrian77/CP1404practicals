836880a8ff8376a80d44b87afbbb5527ddb0e8f6
Dohoon Kim <nabrian77@gmail.com>
2023-04-03 14:36:35 +0800

    Add lists_warmup.py

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,25 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# numbers[0] answer: 3
+# numbers[-1] answer: 2
+# numbers[3] answer: 1
+# numbers[:-1] answer: [3, 1, 4, 1, 5, 9]
+# numbers[3:4] answer: [1]
+# 5 in numbers answer: True
+# 7 in numbers answer: False
+# "3" in numbers answer: False
+# numbers + [6, 5, 3] answer: [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+# Question 1
+numbers[0] = "Ten"
+
+# Question 2
+numbers[-1] = 1
+
+# Question 3
+print(numbers[2:])
+
+# Question 4
+print(9 in numbers)
+
+
