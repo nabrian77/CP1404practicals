c4e26f8d826ac58ade0a0a9c1b90ff63d498b6c5
Dohoon Kim <nabrian77@gmail.com>
2023-04-16 16:08:27 +0800

    Add state_names.py

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -8,11 +8,13 @@ File needs reformatting
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT" : "Northern Territory", "WA" : "Western Australia",
             "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
+for i in CODE_TO_NAME:
+    print("{} is {}" .format(i, CODE_TO_NAME[i]))
 
-state_code = input("Enter short state: ")
+state_code = input("Enter short state: ").upper()
 while state_code != "":
     if state_code in CODE_TO_NAME:
         print(state_code, "is", CODE_TO_NAME[state_code])
     else:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state_code = input("Enter short state: ").upper()
