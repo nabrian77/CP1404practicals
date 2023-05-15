1eddfba08e5469e36d9a4163dcf279745ac50ec5
Dohoon Kim <nabrian77@gmail.com>
2023-05-15 15:33:46 +0800

    Edit taxi_test.py

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,15 @@
+from prac_09.taxi import Taxi
+
+
+def main():
+    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi.drive(40)
+    print(my_taxi)
+    print("Current fare is $",my_taxi.get_fare())
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+    print(my_taxi)
+    print("Current Fare is $",my_taxi.get_fare())
+
+
+main()
